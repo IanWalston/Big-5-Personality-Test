1c66ed7315c3372a242ef43f56c37fb3b9020ef5
Ian Walston <ikwalston@comcast.net>
2019-01-09 00:28:21 -0500

    first

---
+++ questions.js
@@ -0,0 +1,304 @@
+questionsArray = [
+  {
+    questionNumber: "1.",
+    question: "Am the life of the party.",
+    scorecode: "(1+)",
+    reversed: false
+  },
+  {
+    questionNumber: "2.",
+    question: "Feel little concern for others.",
+    scorecode: "(2-)",
+    reversed: true
+  },
+  {
+    questionNumber: "3.",
+    question: "Am always prepared.",
+    scorecode: "(3+)",
+    reversed: false
+  },
+  {
+    questionNumber: "4.",
+    question: "Get stressed out easily.",
+    scorecode: "(4-)",
+    reversed: true
+  },
+  {
+    questionNumber: "5.",
+    question: "Have a rich vocabulary.",
+    scorecode: "(5+)",
+    reversed: false
+  },
+  {
+    questionNumber: "6.",
+    question: "Don't talk a lot.",
+    scorecode: "(1-)",
+    reversed: true
+  },
+  {
+    questionNumber: "7.",
+    question: "Am interested in people.",
+    scorecode: "(2+)",
+    reversed: false
+  },
+  {
+    questionNumber: "8.",
+    question: "Leave my belongings around.",
+    scorecode: "(3-)",
+    reversed: true
+  },
+  {
+    questionNumber: "9.",
+    question: "Am relaxed most of the time.",
+    scorecode: "(4+)",
+    reversed: false
+  },
+  {
+    questionNumber: "10.",
+    question: "Have difficulty understanding abstract ideas.",
+    scorecode: "(5-)",
+    reversed: true
+  },
+  {
+    questionNumber: "11.",
+    question: "Feel comfortable around people.",
+    scorecode: "(1+)",
+    reversed: false
+  },
+  {
+    questionNumber: "12.",
+    question: "Insult people.",
+    scorecode: "(2-)",
+    reversed: true
+  },
+  {
+    questionNumber: "13.",
+    question: "Pay attention to details.",
+    scorecode: "(3+)",
+    reversed: false
+  },
+  {
+    questionNumber: "14.",
+    question: "Worry about things.",
+    scorecode: "(4-)",
+    reversed: true
+  },
+  {
+    questionNumber: "15.",
+    question: "Have a vivid imagination.",
+    scorecode: "(5+)",
+    reversed: false
+  },
+  {
+    questionNumber: "16.",
+    question: "Keep in the background.",
+    scorecode: "(1-)",
+    reversed: true
+  },
+  {
+    questionNumber: "17.",
+    question: "Sympathize with others' feelings.",
+    scorecode: "(2+)",
+    reversed: false
+  },
+  {
+    questionNumber: "18.",
+    question: "Make a mess of things.",
+    scorecode: "(3-)",
+    reversed: true
+  },
+  {
+    questionNumber: "19.",
+    question: "Seldom feel blue.",
+    scorecode: "(4+)",
+    reversed: false
+  },
+  {
+    questionNumber: "20.",
+    question: "Am not interested in abstract ideas.",
+    scorecode: "(5-)",
+    reversed: true
+  },
+  {
+    questionNumber: "21.",
+    question: "Start conversations.",
+    scorecode: "(1+)",
+    reversed: false
+  },
+  {
+    questionNumber: "22.",
+    question: "Am not interested in other people's problems.",
+    scorecode: "(2-)",
+    reversed: true
+  },
+  {
+    questionNumber: "23.",
+    question: "Get chores done right away.",
+    scorecode: "(3+)",
+    reversed: false
+  },
+  {
+    questionNumber: "24.",
+    question: "Am easily disturbed.",
+    scorecode: "(4-)",
+    reversed: true
+  },
+  {
+    questionNumber: "25.",
+    question: "Have excellent ideas.",
+    scorecode: "(5+)",
+    reversed: false
+  },
+  {
+    questionNumber: "26.",
+    question: "Have little to say.",
+    scorecode: "(1-)",
+    reversed: true
+  },
+  {
+    questionNumber: "27.",
+    question: "Have a soft heart.",
+    scorecode: "(2+)",
+    reversed: false
+  },
+  {
+    questionNumber: "28.",
+    question: "Often forget to put things back in their proper place.",
+    scorecode: "(3-)",
+    reversed: true
+  },
+  {
+    questionNumber: "29.",
+    question: "Get upset easily.",
+    scorecode: "(4-)",
+    reversed: true
+  },
+  {
+    questionNumber: "30.",
+    question: "Do not have a good imagination.",
+    scorecode: "(5-)",
+    reversed: true
+  },
+  {
+    questionNumber: "31.",
+    question: "Talk to a lot of different people at parties.",
+    scorecode: "(1+)",
+    reversed: false
+  },
+  {
+    questionNumber: "32.",
+    question: "Am not really interested in others.",
+    scorecode: "(2-)",
+    reversed: true
+  },
+  {
+    questionNumber: "33.",
+    question: "Like order.",
+    scorecode: "(3+)",
+    reversed: false
+  },
+  {
+    questionNumber: "34.",
+    question: "Change my mood a lot.",
+    scorecode: "(4-)",
+    reversed: true
+  },
+  {
+    questionNumber: "35.",
+    question: "Am quick to understand things.",
+    scorecode: "(5+)",
+    reversed: false
+  },
+  {
+    questionNumber: "36.",
+    question: "Don't like to draw attention to myself.",
+    scorecode: "(1-)",
+    reversed: true
+  },
+  {
+    questionNumber: "37.",
+    question: "Take time out for others.",
+    scorecode: "(2+)",
+    reversed: false
+  },
+  {
+    questionNumber: "38.",
+    question: "Shirk my duties.",
+    scorecode: "(3-)",
+    reversed: true
+  },
+  {
+    questionNumber: "39.",
+    question: "Have frequent mood swings.",
+    scorecode: "(4-)",
+    reversed: true
+  },
+  {
+    questionNumber: "40.",
+    question: "Use difficult words.",
+    scorecode: "(5+)",
+    reversed: false
+  },
+  {
+    questionNumber: "41.",
+    question: "Don't mind being the center of attention.",
+    scorecode: "(1+)",
+    reversed: false
+  },
+  {
+    questionNumber: "42.",
+    question: "Feel others' emotions.",
+    scorecode: "(2+)",
+    reversed: false
+  },
+  {
+    questionNumber: "43.",
+    question: "Follow a schedule.",
+    scorecode: "(3+)",
+    reversed: false
+  },
+  {
+    questionNumber: "44.",
+    question: "Get irritated easily.",
+    scorecode: "(4-)",
+    reversed: true
+  },
+  {
+    questionNumber: "45.",
+    question: "Spend time reflecting on things.",
+    scorecode: "(5+)",
+    reversed: false
+  },
+  {
+    questionNumber: "46.",
+    question: "Am quiet around strangers.",
+    scorecode: "(1-)",
+    reversed: true
+  },
+  {
+    questionNumber: "47.",
+    question: "Make people feel at ease.",
+    scorecode: "(2+)",
+    reversed: false
+  },
+  {
+    questionNumber: "48.",
+    question: "Am exacting in my work.",
+    scorecode: "(3+)",
+    reversed: false
+  },
+  {
+    questionNumber: "49.",
+    question: "Often feel blue.",
+    scorecode: "(4-)",
+    reversed: true
+  },
+  {
+    questionNumber: "50.",
+    question: "Am full of ideas.",
+    scorecode: "(5+)",
+    reversed: false
+  }
+];
+
+exports.questions =  questionsArray;
---
+++ scoring.js
@@ -0,0 +1,45 @@
+let questions = require("./questions.js");
+
+//function for scoring the test
+const scoreTest = score => {
+  const quarr = questions.questions;
+
+  const scorearray = score.split(",");
+  //assign each question in array a score value
+  for (let index = 0; index < scorearray.length; index++) {
+    const element = scorearray[index];
+    quarr[index].score = element;
+  }
+
+  const scoredQuestions = quarr;
+
+  const userScore = {
+    Extraversion: 0,
+    Agreeableness: 0,
+    Conscientiousness: 0,
+    Emotional_Stability: 0,
+    Intellect: 0
+  };
+
+  scoredQuestions.forEach(question => {
+    question.scorecode.includes("1")
+      ? (userScore.Extraversion += parseInt(question.score))
+      : question.scorecode.includes("2")
+      ? (userScore.Agreeableness += parseInt(question.score))
+      : question.scorecode.includes("3")
+      ? (userScore.Conscientiousness += parseInt(question.score))
+      : question.scorecode.includes("4")
+      ? (userScore.Emotional_Stability += parseInt(question.score))
+      : question.scorecode.includes("5")
+      ? (userScore.Intellect += parseInt(question.score))
+      : console.log(
+          "ERROR: There was a problem with one of the scores. The results will be flawed."
+        );
+  });
+
+  return userScore;
+};
+
+const samplescore =
+  "1,4,0,1,2,3,0,4,4,0,4,3,3,0,2,4,3,3,1,0,4,1,1,0,2,1,2,4,1,0,3,2,0,0,3,2,4,0,2,3,4,1,3,3,4,4,1,3,1,2";
+console.log(scoreTest(samplescore));
