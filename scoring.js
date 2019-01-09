634208134f715d0b43d99424a2af36777fc07393
Ian Walston <ikwalston@comcast.net>
2019-01-09 00:40:45 -0500

    server.js runs the scoretest function when you make a get request

--- scoring.js
+++ scoring.js
@@ -43,3 +43,5 @@ const scoreTest = score => {
 const samplescore =
   "1,4,0,1,2,3,0,4,4,0,4,3,3,0,2,4,3,3,1,0,4,1,1,0,2,1,2,4,1,0,3,2,0,0,3,2,4,0,2,3,4,1,3,3,4,4,1,3,1,2";
 console.log(scoreTest(samplescore));
+
+exports.score =  scoreTest;
